2d9ae4d3c0b891b9147f8bed60580ee6db90d099
atOhmic <suarez.tomas@gmail.com>
2024-12-10 22:53:59 +0100

    Initial commit

--- app/src/main/java/atohmic/apps/nanamultiply/ui/theme/Theme.kt
+++ app/src/main/java/atohmic/apps/nanamultiply/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package atohmic.apps.nanamultiply.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
